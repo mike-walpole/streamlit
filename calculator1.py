2e4ac0afcc4d94a61373ec304ca88132926c9d40
mike-walpole <walpole.mike@gmail.com>
2022-05-23 17:48:41 +1000

    Update calculator1.py

--- calculator1.py
+++ calculator1.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 
-target = st.number_input('How much do you want to save?', value=int)
+target = st.number_input('How much do you want to save?')
 deposit = st.number_input('How much have you saved already?')
 contr = st.number_input('How much can you save a month?')
 t = st.number_input('in how many years do you want to achieve your goal?')
