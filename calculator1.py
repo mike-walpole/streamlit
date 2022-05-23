cefba8f793560c5738ee34616b05a324979c5b26
mike-walpole <walpole.mike@gmail.com>
2022-05-23 17:43:06 +1000

    Update calculator1.py

--- calculator1.py
+++ calculator1.py
@@ -7,7 +7,7 @@ st.title('Uber pickups in NYC')
 
 target = st.number_input('How much do you want to save?')
 deposit = st.number_input('How much have you saved already?')
-contr = = st.number_input('How much can you save a month?')
+contr = st.number_input('How much can you save a month?')
 t = st.number_input('in how many years do you want to achieve your goal?')
 
 r = 5%
